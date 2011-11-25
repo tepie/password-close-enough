2fafbbc1dcb76b65a604c777e4534dbda3f59af3
tepietrondi <terrence.a.pietrondi@gmail.com>
2011-11-25 11:29:49 -0500

    keyboard character object

---
+++ ios/Classes/KeyboardCharacters.h
@@ -0,0 +1,20 @@
+//
+//  KeyboardCharacters.h
+//  pce
+//
+//  Created by terrence pietrondi on 11/25/11.
+//  Copyright 2011 pietrondi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface KeyboardCharacters : NSObject {
+	NSMutableArray *boardArrays;
+	//NSMutableDictionary *lookupCache;
+}
+
+@property(nonatomic,retain) NSMutableArray *boardArrays;
+-(id) init;
+
+@end
---
+++ ios/Classes/KeyboardCharacters.m
@@ -0,0 +1,29 @@
+//
+//  KeyboardCharacters.m
+//  pce
+//
+//  Created by terrence pietrondi on 11/25/11.
+//  Copyright 2011 pietrondi. All rights reserved.
+//
+
+#import "KeyboardCharacters.h"
+
+
+@implementation KeyboardCharacters
+@synthesize boardArrays;
+
+-(id) init {
+	if (self = [super init]){
+		//[self setBoardArrays : [[[NSMutableArray] alloc] init] ];
+		boardArrays = [[NSMutableArray] alloc] init];
+	}
+	
+	return self;
+}
+
+- (void)dealloc {
+	[boardArrays release];
+    [super dealloc];
+}
+
+@end
