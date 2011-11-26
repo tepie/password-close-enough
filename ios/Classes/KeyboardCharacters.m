2eddb87c2ad1616f2c9ebb9a65448e0c5b085d2e
tepietrondi <terrence.a.pietrondi@gmail.com>
2011-11-26 10:29:12 -0500

    row1

--- ios/Classes/KeyboardCharacters.m
+++ ios/Classes/KeyboardCharacters.m
@@ -14,8 +14,25 @@
 
 -(id) init {
 	if (self = [super init]){
-		//[self setBoardArrays : [[[NSMutableArray] alloc] init] ];
+		
 		boardArrays = [NSMutableArray arrayWithCapacity:3];
+		
+		NSMutableArray *row1 = [NSMutableArray arrayWithCapacity:10];
+		[row1 addObject:  @"q"];
+		[row1 addObject:  @"w"];
+		[row1 addObject:  @"e"];
+		[row1 addObject:  @"r"];
+		[row1 addObject:  @"t"];
+		[row1 addObject:  @"y"];
+		[row1 addObject:  @"u"];
+		[row1 addObject:  @"i"];
+		[row1 addObject:  @"o"];
+		[row1 addObject:  @"p"];
+		 
+		[boardArrays insertObject: row1 atIndex:0];
+		
+		
+		
 	}
 	
 	return self;
